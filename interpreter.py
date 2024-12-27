7efddb12d739258d0d8bd4395017c2b3e04d2897
giorgi babunashvili <90000013+giorgireksi@users.noreply.github.com>
2024-12-27 14:04:15 +0400

    Add files via upload

---
+++ interpreter.py
@@ -0,0 +1,143 @@
+import re
+
+# --- Token Types ---
+INTEGER = 'INTEGER'
+PLUS = 'PLUS'
+MINUS = 'MINUS'
+MUL = 'MUL'
+DIV = 'DIV'
+MOD = 'MOD'
+LPAREN = 'LPAREN'
+RPAREN = 'RPAREN'
+ID = 'ID'
+ASSIGN = 'ASSIGN'
+SEMI = 'SEMI'
+IF = 'IF'
+ELSE = 'ELSE'
+WHILE = 'WHILE'
+PRINT = 'PRINT'
+INPUT = 'INPUT'
+LT = 'LT'
+GT = 'GT'
+LTE = 'LTE'
+GTE = 'GTE'
+EQ = 'EQ'
+NE = 'NE'
+EOF = 'EOF'
+
+# --- Token Class ---
+class Token:
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+    def __str__(self):
+        return f'Token({self.type}, {self.value})'
+
+# --- Lexer ---
+class Lexer:
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+        self.current_char = self.text[self.pos] if self.text else None
+
+    def advance(self):
+        self.pos += 1
+        if self.pos < len(self.text):
+            self.current_char = self.text[self.pos]
+        else:
+            self.current_char = None
+
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def integer(self):
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return int(result)
+
+    def _id(self):
+        result = ''
+        while self.current_char is not None and (self.current_char.isalnum() or self.current_char == '_'):
+            result += self.current_char
+            self.advance()
+        
+        if result.lower() == 'if':
+          return Token(IF, result)
+        elif result.lower() == 'else':
+          return Token(ELSE, result)
+        elif result.lower() == 'while':
+          return Token(WHILE, result)
+        elif result.lower() == 'print':
+          return Token(PRINT, result)
+        elif result.lower() == 'input':
+          return Token(INPUT, result)
+
+        return Token(ID, result)
+
+    def get_next_token(self):
+        while self.current_char is not None:
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
+            if self.current_char.isdigit():
+                return Token(INTEGER, self.integer())
+            if self.current_char.isalpha() or self.current_char == '_':
+                return self._id()
+            if self.current_char == '+':
+                self.advance()
+                return Token(PLUS, '+')
+            if self.current_char == '-':
+                self.advance()
+                return Token(MINUS, '-')
+            if self.current_char == '*':
+                self.advance()
+                return Token(MUL, '*')
+            if self.current_char == '/':
+                self.advance()
+                return Token(DIV, '/')
+            if self.current_char == '%':
+                self.advance()
+                return Token(MOD, '%')
+            if self.current_char == '(':
+                self.advance()
+                return Token(LPAREN, '(')
+            if self.current_char == ')':
+                self.advance()
+                return Token(RPAREN, ')')
+            if self.current_char == '=':
+                self.advance()
+                if self.current_char == '=':
+                    self.advance()
+                    return Token(EQ, '==')
+                return Token(ASSIGN, '=')
+            if self.current_char == ';':
+                self.advance()
+                return Token(SEMI, ';')
+            if self.current_char == '<':
+                self.advance()
+                if self.current_char == '=':
+                    self.advance()
+                    return Token(LTE, '<=')
+                return Token(LT, '<')
+            if self.current_char == '>':
+                self.advance()
+                if self.current_char == '=':
+                    self.advance()
+                    return Token(GTE, '>=')
+                return Token(GT, '>')
+            if self.current_char == '!':
+                self.advance()
+                if self.current_char == '=':
+                    self.advance()
+                    return Token(NE, '!=')
+
+            self.error()
+
+        return Token(EOF, None)
+      
+    def error(self):
+        raise Exception('Invalid character')
